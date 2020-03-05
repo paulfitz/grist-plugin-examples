368566ab7f7335d7aeda2873ed9128d1d8530f87
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2020-03-05 11:11:24 -0500

    update map widget

--- examples/example-6-map/page.js
+++ examples/example-6-map/page.js
@@ -2,14 +2,11 @@
 
 /* global grist, window */
 
-grist.ready();
+let amap;
+let popups = {};
 
-window.onload = function() {
-  const api = grist.rpc.getStub("GristDocAPI@grist", grist.checkers.GristDocAPI);
-  api.listTables()
-    .then(tables => {
-      return api.fetchTable(tables[0]);
-    })
+function forTable(name, rowId) {
+  grist.docApi.fetchSelectedTable()
     .then(data => {
       if (!data) {
         console.log("No table found");
@@ -23,18 +20,39 @@ window.onload = function() {
 	maxZoom: 18,
 	attribution: 'Tiles &copy; Esri &mdash; National Geographic, Esri, DeLorme, NAVTEQ, UNEP-WCMC, USGS, NASA, ESA, METI, NRCAN, GEBCO, NOAA, iPC'
       });
+      if (amap) { amap.remove(); }
       const map = L.map('map', {layers: [tiles]});
       const markers = L.markerClusterGroup();
       const points = [];
+      popups = {};
       for (let i = 0; i < data.id.length; i++) {
+        if (Math.abs(data.lat[i]) < 0.01 && Math.abs(data.long[i]) < 0.01) {
+          continue;
+        }
         const pt = new L.LatLng(data.lat[i], data.long[i]);
         const title = data.Name[i];
         const marker = L.marker(pt, { title  });
         points.push(pt);
 	marker.bindPopup(title);
 	markers.addLayer(marker);
+        popups[data.id[i]] = marker;
       }
       map.addLayer(markers);
-      map.fitBounds(new L.LatLngBounds(points));
+      map.fitBounds(new L.LatLngBounds(points), {maxZoom: 12, padding: [0, 0]});
+      amap = map;
+      if (rowId && popups[rowId]) {
+        var marker = popups[rowId];
+        if (!marker._icon) { marker.__parent.spiderfy(); }
+        marker.openPopup();
+      }
     });
-};
+}
+
+if (grist.on) {
+  grist.on('message', (e) => {
+    return forTable(e.tableId, e.rowId);
+  });
+}
+
+grist.ready();
+
